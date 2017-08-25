98d44550469615ae874fefea4bc783709bb4fb0a
Dan Phillips <dlphillips@gmail.com>
2017-08-25 14:22:52 -0400

    build test

--- server.js
+++ server.js
@@ -206,8 +206,6 @@ app.get("*", function (req, res) {
 
 // -------------------------------------------------
 
-
-
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
